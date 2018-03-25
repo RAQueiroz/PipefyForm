c99ba993293a053447809a0cc27fcf4d67c6895e
diegotsi <diegooliveira.tsi@gmail.com>
2018-03-24 21:25:30 -0300

    Criação da estrutura básica

---
+++ src/index.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+
+class App extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Pipefy Form</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, app)
+
+
+
